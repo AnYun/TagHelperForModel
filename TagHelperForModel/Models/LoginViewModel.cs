88fe39ffd2f84111d0796f1915a438d7f6bfa76c
AnYun <coding@anyun.idv.tw>
2021-08-28 17:36:25 +0800

    新增登入頁面和 Model

---
+++ TagHelperForModel/Models/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagHelperForModel.Models
+{
+    public class LoginViewModel
+    {
+        /// <summary>
+        /// 帳號
+        /// </summary>
+        [Display(Name = "帳號")]
+        [Required(ErrorMessage = "請輸入 {0}")]
+        public string Account { get; set; }
+        /// <summary>
+        /// 密碼
+        /// </summary>
+        [Display(Name = "密碼")]
+        [Required(ErrorMessage = "請輸入 {0}")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
